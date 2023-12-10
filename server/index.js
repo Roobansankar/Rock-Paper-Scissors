1ca9315a61897e0cd9be8ec856ce7a292c174cf9
Rooban sankar <146194005+Roobansankar@users.noreply.github.com>
2023-12-10 16:14:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,35 +1,29 @@
-const express = require('express');
+const express = require("express");
+const { Pool } = require("pg");
 const app = express();
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 
 // Middleware
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-// Router
-const infoRouter = require('./router');
-app.use('/info', infoRouter);
+// Database connection
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "rock paper scissor",
+  password: "rooban@17",
+  port: 5432,
+});
 
-// listen port
-const PORT = process.env.PORT || 5013;
+// Router
+const infoRouter = require("./router");
+app.use("/info", infoRouter);
+
+// Listen port
+const PORT = process.env.PORT || 5015;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
-});
-
-// Db Connection
-mongoose.connect('mongodb://127.0.0.1:27017/mongoose', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.once('open', () => {
-  console.log('Database connection successfully');
-});
-
-db.on('error', (err) => {
-  console.error('Database connection error:', err);
 });
